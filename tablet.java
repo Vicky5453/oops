a0d5731af5469492c66dae4804c88e6912e10b49
Vicky5453 <113967961+Vicky5453@users.noreply.github.com>
2022-10-04 13:14:11 +0530

    Add files via upload

---
+++ tablet.java
@@ -0,0 +1,28 @@
+class tablet
+{
+int a=100,b=200;
+void add()
+{
+int c=a+b;
+System.out.println(c);
+}
+void sub()
+{
+int c=a-b;
+System.out.println(c);
+}
+void mul()
+{
+int c=a*b;
+System.out.println(c);
+}
+void div()
+{
+int c=a/b;
+System.out.println(c);
+}
+public static void main(String [] args)
+{
+tablet vivo=new tablet();
+}
+}
