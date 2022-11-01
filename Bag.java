fa8cceb34c3d0ec42ba31decba443155fdfa59c6
Vicky5453 <113967961+Vicky5453@users.noreply.github.com>
2022-11-01 13:31:08 +0530

    Add files via upload

---
+++ Bag.java
@@ -0,0 +1,17 @@
+class Bag
+{
+String price="500";
+int model;
+String color="black";
+void storebook()
+{
+System.out.println("storing the book");
+}
+public static void main(String [] args)
+{
+Bag student = new Bag();
+student.storebook();
+System.out.println("price+model+color");
+}
+}
+
