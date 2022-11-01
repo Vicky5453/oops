d3313e78d347882dc369be49eaf77a2b97ae0649
Vicky5453 <113967961+Vicky5453@users.noreply.github.com>
2022-11-01 13:29:48 +0530

    Add files via upload

---
+++ Babybird.java
@@ -0,0 +1,11 @@
+class Babybird extends Bird
+{
+public static void main (String [] args)
+{
+Babybird tree = new Babybird();
+tree.sleep();
+tree.fly();
+System.out.println("singing+color");
+}
+}
+
---
+++ Bird.java
@@ -0,0 +1,14 @@
+class Bird
+{
+int sing;
+String color="bink";
+void sleep()
+{
+System.out.println("sleeping");
+}
+public void fly()
+{
+System.out.println("flying");
+}
+}
+
