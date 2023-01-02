c73acf6f2274e41f98b340ec41e913d97794420e
Vicky5453 <113967961+Vicky5453@users.noreply.github.com>
2023-01-02 19:00:52 +0530

    Add files via upload

---
+++ Hai.java
@@ -0,0 +1,9 @@
+package india.chennai;    // sub folder  command compile---> java -d . classname.java
+public class Hai                   //    command run    ---> java india.chennai.classname      
+{
+public static void main (String [] args)
+{
+System.out.println("hello");
+}
+}
+
