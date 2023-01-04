2befb6824a99cfe7c9cc814791fc4d85b72882c1
Vicky5453 <113967961+Vicky5453@users.noreply.github.com>
2023-01-04 18:37:13 +0530

    Add files via upload

---
+++ protected method/Gasstove.java
@@ -0,0 +1,25 @@
+package us;
+public class Gasstove {
+
+	public static void main(String[] args) {
+
+	}
+protected void avoid()
+{
+System.out.println(" avoid explosive");
+}
+	
+	public void cook()
+	{
+		System.out.println("cooking");
+	}
+	public void preparefood()
+	{
+		System.out.println("prepare cooking");
+	}
+	public void heatwater()
+	{
+		System.out.println("heat of water");
+	}
+
+}
---
+++ protected method/Inductionstove.java
@@ -0,0 +1,18 @@
+package france;
+import us.*;
+public class Inductionstove extends Gasstove {
+
+	public static void main(String[] args) {
+		
+		Inductionstove in = new Inductionstove();
+		in.cook();
+		in.preparefood();
+		in.heatwater();
+        in.avoid();
+		
+		
+
+	}
+
+}
+
