5a6040fe08f9f70e96d58e589aaa3fab9c03502f
Vicky5453 <113967961+Vicky5453@users.noreply.github.com>
2023-01-10 13:48:51 +0530

    Add files via upload

---
+++ getter method/Cognizant_chennai.java
@@ -0,0 +1,16 @@
+package getterMethod;
+
+public class Cognizant_chennai {
+	
+	private int salary=200000;
+	
+//	public int salary=100000;
+	
+	public int getSalary()
+	{
+		return this.salary;
+	}
+
+	
+
+}
---
+++ getter method/Employee.java
@@ -0,0 +1,21 @@
+package getterMethod;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		
+		Cognizant_chennai emp = new Cognizant_chennai();
+		
+	//	System.out.println(emp.salary);
+		
+		System.out.println(emp.getSalary());
+		
+	//	emp.salary=300000;
+		
+	//	System.out.println(emp.salary);
+		
+
+
+	}
+
+}
